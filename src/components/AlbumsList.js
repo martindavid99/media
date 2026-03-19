a7d87ca30bbf61a13c23b92ea6dd39f1ef290ce1
David <martindavid99@gmail.com>
2026-03-18 20:45:45 -0500

    albums and header

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,11 +1,35 @@
 import { useFetchAlbumsQuery } from "../store/apis/albumsApi";
+import Skeleton from './Skeleton';
+import ExpandablePanel from './ExpandablePanel';
+import Buttton from './Button';
 
 function AlbumsList({ user }) {
     const { data, error, isLoading } = useFetchAlbumsQuery(user);
 
-    console.log(data, error, isLoading);
+    let content;
+    if (isLoading) {
+        content = <Skeleton times={3} />;
+    } else if (error) {
+        content = <div>Error loading albums.</div>;
+    } else {
+        content = data.map(album => {
+            const header = <div>{album.title}</div>;
 
-    return <div>Albums for {user.name}</div>;
+            return <ExpandablePanel key={album.id} header={header}>
+                List of photos in the album
+            </ExpandablePanel>;
+        });
+    }
+
+
+    return <div>
+        <div>
+            Albums for {user.name}
+        </div>
+        <div>
+            {content}
+        </div>
+    </div>;
 }
 
 export default AlbumsList;
