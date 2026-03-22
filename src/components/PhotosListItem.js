ff92934f0a742855e3524a4438814301da18376a
David <martindavid99@gmail.com>
2026-03-22 01:00:05 -0500

    adding photos

--- src/components/PhotosListItem.js
+++ src/components/PhotosListItem.js
@@ -1,5 +1,7 @@
-function PhotosListItem() {
-    return 'PhotosListItem';
+function PhotosListItem({ photo }) {
+    return <div className="relative m-2">
+        <img className="h-20 w-20" src={photo.url} alt="random pic" />
+    </div>;
 }
 
 export default PhotosListItem;
